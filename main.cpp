1b945a3d771c756a5c246e9e4a36516fe0e996af
jack5252tw <jack5252tw@gmail.com>
2019-05-01 01:40:11 -0700

    bug fixed

--- main.cpp
+++ main.cpp
@@ -40,13 +40,13 @@ int main(void) {
   int third = store[6]*4 + store[7]*2 + store[8]*1;
   int fourth = store[9]*4 + store[10]*2 + store[11]*1;
   int sum = first+second+third+fourth;
-  printf("%c\r\n",sum);
+  printf("%d\r\n",sum);
   ////////////////////////////
   int out[8];
   out[0] = 0;
   out[1] = 0;
   out[2] = 0;
-  
+
   if(sum >= 16){
       out[3] = 1;
   }
@@ -62,7 +62,7 @@ int main(void) {
       out[4] = 0;
   }
 
-  int tmp = tmp-out[1]*8;
+  tmp = tmp-out[1]*8;
   if( tmp >= 4){
       out[5] = 1;
   }
@@ -70,7 +70,7 @@ int main(void) {
       out[5] = 0;
   }
 
-  int tmp = tmp-out[2]*4;
+  tmp = tmp-out[2]*4;
   if( tmp >= 2){
       out[6] = 1;
   }
@@ -78,11 +78,14 @@ int main(void) {
       out[6] = 0;
   }
   
-  int tmp = tmp-out[3]*2;
+  tmp = tmp-out[3]*2;
   if( tmp >= 1){
       out[7] = 1;
   }
   else{
       out[7] = 0;
+  }
+  for(int z = 0; z <8; z++){
+    printf("%d",out[z]);
   }
 }
