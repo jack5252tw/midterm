513886531cd38a023795c743003e12b701b00a13
jack5252tw <jack5252tw@gmail.com>
2019-05-01 01:37:55 -0700

    try binary

--- main.cpp
+++ main.cpp
@@ -40,5 +40,49 @@ int main(void) {
   int third = store[6]*4 + store[7]*2 + store[8]*1;
   int fourth = store[9]*4 + store[10]*2 + store[11]*1;
   int sum = first+second+third+fourth;
-  printf("%d\r\n",sum);
+  printf("%c\r\n",sum);
+  ////////////////////////////
+  int out[8];
+  out[0] = 0;
+  out[1] = 0;
+  out[2] = 0;
+  
+  if(sum >= 16){
+      out[3] = 1;
+  }
+  else{
+      out[3] = 0;
+  }
+
+  int tmp = sum-out[0]*16;
+  if( tmp >= 8){
+      out[4] = 1;
+  }
+  else{
+      out[4] = 0;
+  }
+
+  int tmp = tmp-out[1]*8;
+  if( tmp >= 4){
+      out[5] = 1;
+  }
+  else{
+      out[5] = 0;
+  }
+
+  int tmp = tmp-out[2]*4;
+  if( tmp >= 2){
+      out[6] = 1;
+  }
+  else{
+      out[6] = 0;
+  }
+  
+  int tmp = tmp-out[3]*2;
+  if( tmp >= 1){
+      out[7] = 1;
+  }
+  else{
+      out[7] = 0;
+  }
 }
